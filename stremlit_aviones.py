e4feb037a37afdae37c2ea046739e59517c37312
Ignacio <165803275+nvchoAf@users.noreply.github.com>
2024-11-24 22:15:58 -0300

    i

--- stremlit_aviones.py
+++ stremlit_aviones.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1woQmfP8of-9Sq2F73uKWzjn_MX8o7yOI
 """
 
-}
 # prompt: creame un stremlit que pueda mostrar mi base de datos
 
 import streamlit as st
